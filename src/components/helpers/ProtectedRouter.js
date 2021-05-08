e9c02807c0e589f04966131f866f429ef64f7505
ciacelo <ciacelojr@gmail.com>
2021-05-08 20:19:30 -0300

    added component of  router protection

---
+++ src/components/helpers/ProtectedRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Route } from 'react-router';
+import Login from '../login';
+import { UserContext } from '../UserContext';
+
+const ProtectedRouter = props => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
+};
+
+export default ProtectedRouter;
