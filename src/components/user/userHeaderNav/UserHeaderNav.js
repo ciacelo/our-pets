b48332d43825dd7ffa1907242d4f81aa86c2f595
ciacelo <ciacelojr@gmail.com>
2021-05-17 22:03:24 -0300

    created the useMedia custom hooks, to return resizing of the app

--- src/components/user/userHeaderNav/UserHeaderNav.js
+++ src/components/user/userHeaderNav/UserHeaderNav.js
@@ -6,29 +6,42 @@ import { ReactComponent as MyPhotos } from '../../../assets/feed.svg';
 import { ReactComponent as Stats } from '../../../assets/estatisticas.svg';
 import { ReactComponent as AddPhoto } from '../../../assets/adicionar.svg';
 import { ReactComponent as Exit } from '../../../assets/sair.svg';
+import useMedia from '../../../hooks/useMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
   return (
-    <nav id="user-header-nav" className="nav">
-      <NavLink to="/conta" end activeClassName="active">
-        <MyPhotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/conta/estatisticas" activeClassName="active">
-        <Stats />
-        {mobile && 'Estatisticas'}
-      </NavLink>
-      <NavLink to="/conta/postar" activeClassName="active">
-        <AddPhoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Exit />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          className="mobileButton"
+          id="user-header-nav-mobileButton"
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav id="user-header-nav" className="nav">
+        <NavLink to="/conta" end activeClassName="active">
+          <MyPhotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas" activeClassName="active">
+          <Stats />
+          {mobile && 'Estatisticas'}
+        </NavLink>
+        <NavLink to="/conta/postar" activeClassName="active">
+          <AddPhoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Exit />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
