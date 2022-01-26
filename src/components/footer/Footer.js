f396533a8962f7c7b7c8ac3c2a14c085f051108c
ciacelo <ciacelojr@gmail.com>
2022-01-26 15:35:59 -0300

    atualizando footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import './footer.scss';
+import {ReactComponent as Dogs} from '../../assets/dogs-footer.svg'
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className='footer'>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
